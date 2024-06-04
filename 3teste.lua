7ea18059115a18aa8a2748d588deb2aba4cb3136
SamuelHenriqueDeMoraisVitrio <samuelhenriquemoraisvitrio@gmail.com>
2024-06-04 13:33:16 -0300

    Testando em lua o send_text

--- 3teste.lua
+++ 3teste.lua
@@ -4,8 +4,9 @@ set_server.single_process = true
 
 local function main(request)
 
+    local header = request.header[1]
 
-    print("Teste do send_text")
+    print("Teste do send_text", header.key)
 
     return serjao.send_text("O send_text está funcionando", 200)
 end
