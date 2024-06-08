174af3fa8e2ce2e068afd960453ce0367c047728
SamuelHenriqueDeMoraisVitrio <samuelhenriquemoraisvitrio@gmail.com>
2024-06-08 15:35:20 -0300

    lista e single_process

--- teste.lua
+++ teste.lua
@@ -8,7 +8,15 @@ local function teste(request)
                 serjao.kill()
         end
 
-        return serjao.send_text("html", 200)
+        local pagina  =html(
+                body(
+                        h2({id="teste",style={color="yellow"}},"aaa"),
+                        h1({style={color="blue"}},"eai parsa")
+                ),
+                serjao.component("h4"),
+                serjao.fragment("<area>")
+        )
+        return serjao.send_html(pagina,200)
 
 end
 
