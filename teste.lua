5cedc5e890d14d290df9a9c90caf631ec38984c1
SamuelHenriqueDeMoraisVitrio <samuelhenriquemoraisvitrio@gmail.com>
2024-06-08 16:39:53 -0300

    zipado

--- teste.lua
+++ teste.lua
@@ -6,10 +6,7 @@ set_server.single_process = true
 
 ---@param request Request
 local function teste(request)
-
-     
       return serjao.send_text("aaa",200)
-
 end
 
 serjao.initserver(3000,4000,teste);
