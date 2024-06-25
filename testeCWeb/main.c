adca8445837fa4e6d8d80c70f62aec45f326d54c
SamuelHenriqueDeMoraisVitrio <samuelhenriquemoraisvitrio@gmail.com>
2024-06-25 18:38:48 -0300

    testeCWEB

--- testeCWeb/main.c
+++ testeCWeb/main.c
@@ -5,6 +5,10 @@ CwebNamespace cb;
 CwebHttpResponse *main_sever(CwebHttpRequest *request ){
 
 
+    if(strcmp(request->route, "/html") == 0){
+
+    }
+
     if(strcmp(request->route,"/kill") == 0){
         cweb_kill_single_process_server();
     }
@@ -16,6 +20,8 @@ int main(int argc, char *argv[]){
     cb = newCwebNamespace();
     CwebServer server = newCwebSever(3001, main_sever);
     server.single_process = true;
+    server.use_static = true;
+    server.static_folder = "front";
     cb.server.start(&server);
     return 0;
 }
