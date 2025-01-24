445fcc801bbd5b9cb40dc615799c616ee29680ad
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-01-24 15:09:43 -0300

    fix

--- src/functions/html_engine/declaration.h
+++ src/functions/html_engine/declaration.h
@@ -1,4 +1,4 @@
 #include "components/components.h"
 #include "shortcuts/shortcuts.h"
 #include "props/props.h"
-#include "commandsHTML/commands<|>.h"
+#include "commandsHTML/commands.h"
--- src/functions/html_engine/definition.h
+++ src/functions/html_engine/definition.h
@@ -1,4 +1,4 @@
 #include "components/components.c"
 #include "shortcuts/shortcuts.c"
 #include "props/props.c"
-#include "commandsHTML/commands<|>.c"
+#include "commandsHTML/commands.c"
